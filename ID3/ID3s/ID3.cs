c68f0b3e7fbc0020c6b1ea97d434c0c035442ea2
danielx <danielx-25@hotmail.com>
2016-12-19 00:13:53 -0300

    arreglando

--- ID3/ID3s/ID3.cs
+++ ID3/ID3s/ID3.cs
@@ -51,7 +51,7 @@ namespace ID3.ID3s
                 raiz = new Nodo(aSalida);
                 return raiz;
             }
-            if (atributos.Count == 0)//si no existe ningun atributo
+            if (atributos.Count == 1)//si no existe ningun atributo
             {
                 //elAtributoSAlidoMayorNumero()
                 //el mayor numero de valor de salida
